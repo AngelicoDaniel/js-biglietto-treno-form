b3fd7cfe25af9fb49f7a1a555fa3928b174876a3
Daniel <angelicodaniel93@gmail.com>
2022-10-20 14:54:05 +0200

    first commit

---
+++ assets/main.js
@@ -0,0 +1,22 @@
+function generaPrezzo(){
+
+    const MoneyPerKm = '0.21';
+    let valueKm = document.getElementById('km').value;
+    let valueage = document.getElementById('age').value;
+    let price = valueKm * MoneyPerKm;
+    
+    
+    if (valueage <= 18) {
+        price = price * 0.8;
+        console.log('minorenne')
+    } else if (valueage >= 65){
+        price = price * 0.6;
+        console.log('over65')
+    } else {
+        finalPrice = price
+        console.log('default')
+    }
+    
+    document.getElementById('finalPrice').innerHTML = `il prezzo finale è: ${price.toFixed(2)}`
+    
+    }
