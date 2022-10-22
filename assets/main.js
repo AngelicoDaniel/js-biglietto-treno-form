19fd1ed74373db0c984cd7b4496302d3274900aa
Daniel <angelicodaniel93@gmail.com>
2022-10-22 10:58:07 +0200

    last commit

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,9 @@
 function generaBiglietto(){
 
     const MoneyPerKm = '0.21';
-    let name = document.getElementById('name').value;
     let valueKm = document.getElementById('km').value;
     let valueage = document.getElementById('age').value;
     let price = valueKm * MoneyPerKm;
-       
     
     if (valueage == 'minorenne') {
         price = price * 0.8;
@@ -22,7 +20,8 @@ function generaBiglietto(){
     document.getElementById('finalPrice').innerHTML = ` ${price.toFixed(2)} €`
 
     //nome e cognome
-    
+    let name = document.getElementById('name').value;
+
     document.getElementById('nome').innerHTML = `${name}`
 
     //carrozza
@@ -36,6 +35,8 @@ function generaBiglietto(){
     document.getElementById('CPcode').innerHTML = `${CPcode}`
 
     }
+
+//funzione ticket reset
 
 function ticketReset(){
     document.getElementById('result').value = ''
